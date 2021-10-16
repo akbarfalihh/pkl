65966202c67e8a195ef485b0d4c1053de8563a1d
akbarfalihh <akbarfalih19@students.undip.ac.id>
2021-10-16 21:03:43 +0700

    coba fix fontawesome-free

--- public/stylecss/fontawesome-free/js/conflict-detection.js
+++ public/stylecss/fontawesome-free/js/conflict-detection.js
@@ -5,7 +5,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (factory((global['fontawesome-free-conflict-detection'] = {})));
+  (factory((global['fontawesome-free-conflict-detection'] == {})));
 }(this, (function (exports) { 'use strict';
 
   function _typeof(obj) {
